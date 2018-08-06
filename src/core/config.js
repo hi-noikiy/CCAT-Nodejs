b16fcc63ee3c0db24769f6fe05ae17606e06e946
paulplayergg <paulplayergg@gmail.com>
2018-08-06 16:53:35 +0800

    redesign for open source

--- src/core/config.js
+++ src/core/config.js
@@ -3,7 +3,7 @@
 const debug = require('debug')('core:config')
 const fs = require('fs')
 const path = require('path')
-const configFile = path.join(__dirname, '../config.json')
+const configFile = path.resolve(process.cwd(), './config.json')
 const config = JSON.parse(fs.readFileSync(configFile))
 
 debug('config.js is called')
@@ -25,22 +25,5 @@ module.exports = {
     name: config.coinbig.name,
     key: config.coinbig.key,
     secret: config.coinbig.secret
-  },
-  register: {
-    user: config.register.user,
-    email: config.register.email,
-    phone: config.register.phone,
-    pwd: config.register.pwd,
-    series: config.register.series,
-    regCode: config.register.regCode
-  },
-  crypto: {
-    privateKey: '',
-    publicKey: `-----BEGIN PUBLIC KEY-----
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDFWnl8fChyKI/Tgo1ILB+IlGr8
-ZECKnnO8XRDwttBbf5EmG0qV8gs0aGkh649rb75I+tMu2JSNuVj61CncL/7Ct2kA
-Z6CZZo1vYgtzhlFnxd4V7Ra+aIwLZaXT/h3eE+/cFsL4VAJI5wXh4Mq4Vtu7uEje
-ogAOgXACaIqiFyrk3wIDAQAB
------END PUBLIC KEY-----`
   }
 }
--- src/core/index.js
+++ src/core/index.js
@@ -1,5 +1,5 @@
 'use strict'
 
 const debug = require('debug')('core:index')
-const path = require('path')
-const config = path.join(__dirname, './config.js')
+
+// for cli, to be continue
